245a0f131b3c6bb8a4a59608897012fe17563719
Mateus Levi <mateus.martins@amlconsulting.com.br>
2019-10-09 10:28:20 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/nelioalves/cursomc/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/nelioalves/cursomc/exceptions/ObjectNotFoundException.java
@@ -1,7 +1,8 @@
 package com.nelioalves.cursomc.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException {
-	
+	private static final long serialVersionUID = 1L;
+
 	public ObjectNotFoundException(String msg) {
 		super(msg);
 	}
