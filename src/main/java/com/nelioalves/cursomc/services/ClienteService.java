10283fda0debc0493a8e4b0d72bcaf9ad2533e9a
Mateus <mateus.martins@amlconsulting.com.br>
2019-10-28 15:53:11 -0300

    Adicionando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -11,15 +11,15 @@ import com.nelioalves.cursomc.repositories.ClienteRepository;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
-	
-	public Cliente buscar(Integer id) {
+
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Cliente.class.getName());
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
 		});
 	}
 }
